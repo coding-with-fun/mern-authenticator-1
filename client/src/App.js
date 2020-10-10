a2d23e37fa2a40d1812b22c9162c7ed40203a0b5
Harsh Patel <harsh@Harshs-MacBook-Air.local>
2020-10-11 00:10:39 +0530

    Added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import PageNotFound from "./components/PageNotFound";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!!</h1>
-    </div>
+    <Fragment>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path="/">
+            <Body />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </Fragment>
   );
 }
 
