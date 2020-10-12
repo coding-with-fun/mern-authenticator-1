3969b7a18dc806a958dac53c1afbc70a7fe3a2c3
Harsh Patel <harsh@Harshs-MacBook-Air.local>
2020-10-12 12:51:09 +0530

    Edited 404 page

--- client/src/components/PageNotFound.js
+++ client/src/components/PageNotFound.js
@@ -4,7 +4,8 @@ import PageNotFoundImg from "../assets/404.jpg";
 const PageNotFound = () => {
   return (
     <div className="page_not_found">
-      <img src={PageNotFoundImg} alt="" />
+      <h1>404</h1>
+      <h3>Page not found</h3>
     </div>
   );
 };
